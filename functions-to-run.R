dc0906c69b5e61d58820ecc4616d611835bc539d
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2019-09-12 09:31:30 +0100

    Wrapping checks into two functions

--- functions-to-run.R
+++ functions-to-run.R
@@ -1,29 +1,38 @@
 # FUNCTIONS TO RUN
 
 # Datafile
-comp_col_check(dataset)
-time_period_check(dataset)
-time_period_check_consecutive(dataset)
-time_identifier_mix(dataset)
-time_identifier_check(dataset)
-comma_check(dataset)
+
+datafile_checks <- function(data) {
+  comp_col_check(data)
+  time_period_check(data)
+  time_period_check_consecutive(data)
+  time_identifier_mix(data)
+  time_identifier_check(data)
+  comma_check(data)
+}
+
+datafile_checks(dataset)
 # NEEDS WORK - geography_level_check(dataset)
 
 # Metadata
-meta_col_check(metadata)
-comma_check(metadata)
-meta_name_check(metadata)
-meta_duplicate_check(metadata)
-meta_name_spaces_check(dataset)
-comp_col_check_meta(metadata)
-col_type_check(metadata)
-meta_label_check(metadata)
-meta_duplicate_label_check(metadata)
-meta_indicator_group_check(metadata)
+metadata_checks <- function(data) {
+  meta_col_check(data)
+  comma_check(data)
+  meta_name_check(data)
+  meta_duplicate_check(data)
+  meta_name_spaces_check(dataset)
+  comp_col_check_meta(data)
+  col_type_check(data)
+  meta_label_check(data)
+  meta_duplicate_label_check(data)
+  meta_indicator_group_check(data)
 # NEEDS WORK - meta_indicator_unit_check(metadata)
-meta_filter_unit_check(metadata)
-meta_filter_hint_check(metadata)
-meta_filter_group_check(metadata)
+  meta_filter_unit_check(data)
+  meta_filter_hint_check(data)
+  meta_filter_group_check(data)
+}
+metadata_checks(metadata)
+
 
 # Cross-validation - could possible split into the above if the variables are set up right at the start
 # as will only need access to the variables and one file
