402371d267053f4caeec5ee835eaf9c6e4ef811d
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2019-09-13 12:16:25 +0100

    Adding trailing white space so it can be tested for properly

--- run.R
+++ run.R
@@ -4,8 +4,8 @@
 library(tidyverse)
 
 # Load your files
-dataset <- read_csv("data_metadata/dynamic_test_data.csv")
-metadata <- read_csv("data_metadata/dynamic_test_data.meta.csv")
+dataset <- read_csv("data_metadata/dynamic_test_data.csv",trim_ws = FALSE)
+metadata <- read_csv("data_metadata/dynamic_test_data.meta.csv",trim_ws = FALSE)
 
 # Build the screener functions
 source("setup_file.R")
