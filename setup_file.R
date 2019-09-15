968261c68bc07af1a215606fcd14d53b904b57e1
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2019-09-15 12:23:43 +0100

    amending error in previous commit

--- setup_file.R
+++ setup_file.R
@@ -7,7 +7,7 @@ data_comp_col <- function(data) {
   comp_col <- c("geographic_level","time_period","time_indentifier")
   for(i in comp_col){
     if(i %in% names(data)){message("PASS - ",i," is present in the data file.")}
-    else{"FAIL - The ",i," variable is missing from the data file."}
+    else{message("FAIL - The ",i," variable is missing from the data file.")}
   }
 }
 
