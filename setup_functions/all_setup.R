63ff5f7bbf04e77e32416fe93c0d42710584251e
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2019-09-18 14:06:41 +0100

    testing the changes in this branch and making it work

--- setup_functions/all_setup.R
+++ setup_functions/all_setup.R
@@ -3,17 +3,17 @@
 # -------------------------------------
 # Combining each setup script
 
-screening_tests <- function(data,meta) {
-  # running the files
-  source('setup_functions/data_filters_setup.r')
-  source('setup_functions/data_general_setup.r')
-  source('setup_functions/data_geography_setup.r')
-  source('setup_functions/data_time_setup.r')
-  source('setup_functions/meta_filter_indicator_setup.r')
-  source('setup_functions/meta_general_setup.r')
-  source('setup_functions/meta_variable_label_setup.r')
+# running the files
+source('setup_functions/data_filters_setup.r')
+source('setup_functions/data_general_setup.r')
+source('setup_functions/data_geography_setup.r')
+source('setup_functions/data_time_setup.r')
+source('setup_functions/meta_filter_indicator_setup.r')
+source('setup_functions/meta_general_setup.r')
+source('setup_functions/meta_variable_label_setup.r')
 
-  # adding the functions to run against data
+# adding the functions to run against data
+screening_tests <- function(data,meta) {
   data_filters_setup(data,meta)
   data_general_setup(data)
   data_geography_setup(data)
