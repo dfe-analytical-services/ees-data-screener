97e5c78e4ede59590dbe6b76432dd4fe9158ac2d
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2019-11-27 12:30:48 +0000

    Adding rmarkdown to the packages needed

--- setup_functions/environment_setup.R
+++ setup_functions/environment_setup.R
@@ -16,6 +16,7 @@ cam_envrionment_setup <- function(){
   if(!require(readr)){install.packages("readr");require(readr)}
   if(!require(stringr)){install.packages("stringr");require(stringr)}
   if(!require(dplyr)){install.packages("dplyr");require(dplyr)}
+  if(!require(rmarkdown)){install.packages("rmarkdown");require(rmarkdown)}
   print("Your environment has successfully been setup, you should now be able to run the screener.")
 }
 cam_envrionment_setup()
