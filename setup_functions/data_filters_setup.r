c639c062b87ca4ab5150cde31c7f184fae266bce
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2019-12-11 21:16:35 +0000

    adding assign

--- setup_functions/data_filters_setup.r
+++ setup_functions/data_filters_setup.r
@@ -9,7 +9,7 @@ data_filters_setup <-function(data,meta){
 }
 
 data_filters_results_function <- function(){
-  data_filters_results <- c(filter_levels_check_result, total_check_result, observational_total_check_result)
+  assign("data_filters_results",c(filter_levels_check_result, total_check_result, observational_total_check_result),envir = .GlobalEnv)
 }
 
 # -------------------------------------
