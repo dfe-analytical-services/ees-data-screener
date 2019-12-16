5c538cf7f596182562a2b5a8d4dc8426b03cfb93
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2019-12-16 13:32:38 +0000

    adding the %  work to the data_comp_col function

--- setup_functions/data_general_setup.r
+++ setup_functions/data_general_setup.r
@@ -9,14 +9,29 @@ data_general_setup <- function(data){
   data_spaces_check(data)
 }
 
+data_general_results_function <- function(){
+  assign("data_general_results",c(data_comp_col_result),envir = .GlobalEnv)
+}
+
 # -------------------------------------
 # check that the compulsory columns exist
 
 data_comp_col <- function(data) {
   comp_col <- c("geographic_level","time_period","time_identifier")
+  data_comp_col_preresult <- c()
   for(i in comp_col){
-    if(i %in% names(data)){message("PASS - ",i," is present in the data file.")}
-    else{message("FAIL - The ",i," variable is missing from the data file.")}
+    if(i %in% names(data)){
+      data_comp_col_preresult[i] <- TRUE
+    }else{
+      message("FAIL - The ",i," variable is missing from the data file.")
+      data_comp_col_preresult[i] <- FALSE
+      }
+  }
+  if(FALSE %in% data_comp_col_preresult){
+    assign("data_comp_col_result",FALSE,envir = .GlobalEnv)
+  }else{
+    message("PASS - time_period, time_identifier, and geographic_level are present.")
+    assign("data_comp_col_result",TRUE,envir = .GlobalEnv)
   }
 }
 
