cf2e5d4f38f0e4423f75572cb5db2fb553723d2d
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-01-02 12:13:18 +0000

    finishing off the refactoring

--- run.R
+++ run.R
@@ -6,7 +6,7 @@ source('setup_functions/environment_setup.R')
 # The following line will check your version of Pandoc and automatically update it for you if needed.
 # If it needs updating, this may take a couple of minutes. It will download it for you and automatically start the install wizard.
 # Follow the instructions for the install, restart RStudio, and then run this again, it won't redownload pandoc if you have version 2.7.3 or later.
-cam_pandoc_install()
+pandoc_install()
 
 # Ensure the data file and corresponding metadata file you want to screen are saved in the data_metadata folder.
 # Enter the name of the data file below, note that you do not need to include the extension '.csv'.
--- setup_functions/environment_setup.R
+++ setup_functions/environment_setup.R
@@ -3,14 +3,14 @@
 # -------------------------------------
 
 # function to check the pandoc version and install the latest if not 2.7.3 or later
-cam_pandoc_install <- function(){
+pandoc_install <- function(){
   if(rmarkdown::pandoc_version()>='2.7.3'){print("You already have version 2.7.3 or later of Pandoc installed, you're good to use the screener.")}
   else{if(!require(installr)){install.packages("installr");require(installr)}
   install.pandoc()}
 }
 
 # function to install/load the packages needed to run the screener
-cam_envrionment_setup <- function(){
+envrionment_setup <- function(){
   if(!require(govdown)){install.packages("govdown");require(govdown)}
   if(!require(tidyr)){install.packages("tidyr");require(tidyr)}
   if(!require(readr)){install.packages("readr");require(readr)}
@@ -20,7 +20,7 @@ cam_envrionment_setup <- function(){
   if(!require(knitr)){install.packages("knitr");require(knitr)}
   print("Your environment has successfully been setup, you should now be able to run the screener.")
 }
-cam_envrionment_setup()
+envrionment_setup()
 
 # hard-coded variables
 acceptable_time_identifiers <- c("Spring term","Autumn term","Autumn and spring term",
