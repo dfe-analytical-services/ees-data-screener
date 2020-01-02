a8cbf174fa01d9fe9c35beddae8bc13f6f0d9d25
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-01-02 13:16:04 +0000

    Something else I'd missed...

--- setup_functions/function_setup.r
+++ setup_functions/function_setup.r
@@ -71,7 +71,7 @@ meta_filter_results_function()
 meta_indicator_results_function()
 
 data_results <- c(data_filter_results,data_general_results,data_geography_results,data_time_results)
-meta_results <- c(meta_filter_indicator_results,meta_general_results,meta_variable_label_results)
+meta_results <- c(meta_filter_results,meta_indicator_results,meta_general_results,meta_variable_results,meta_label_results)
 all_results <- c(data_results,meta_results)
 
 pass <- length(which(all_results==TRUE))
