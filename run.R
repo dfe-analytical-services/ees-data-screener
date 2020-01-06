f8e09a0699ed8970842ca336071475f78d20e350
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-01-06 12:00:15 +0000

    Cleaning up the merge faff

--- run.R
+++ run.R
@@ -8,16 +8,5 @@ source("setup_functions/setup_environment.R", encoding = "UTF-8")
 # Follow the instructions for the install, restart RStudio, and then run this again, it won't redownload pandoc if you have version 2.7.3 or later.
 pandoc_install()
 
-<<<<<<< HEAD
 # Screen your files by running the following line and responding to the prompts.
 screening_results()
-=======
-# Ensure the data file and corresponding metadata file you want to screen are saved in the data_metadata folder.
-# Enter the name of the data file below, note that you do not need to include the extension '.csv'.
-# The metadata file will automatically be recognised if you have followed the naming convention - mydatafilename.meta.csv.
-your_data_file <- "all_geographies_cam_EES-770"
-
-# Run the report.
-rmarkdown::render("EES-data-screener-report.Rmd",
-                  output_file = paste(gsub(":",".",gsub("\\s","_",paste(your_data_file,"_","report_", Sys.time(),'.html',sep='')))))
->>>>>>> master
