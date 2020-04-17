3167f34fc04500226853094fc85e59b1c91d9e2f
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-04-17 16:44:55 +0100

    updating messaging on 3 years test

--- setup_functions/data_time_setup.r
+++ setup_functions/data_time_setup.r
@@ -293,7 +293,9 @@ time_identifier_mix <- function(data) {
 
 three_years <- function(data) {
   if (length(unique(data$time_period)) < 3) {
-    message("ADVISORY - Your file contains fewer than 3 years - where it exists, you should include at least 3 years of data in your file to meet the upcoming change in accessibility legislation.")
+    message("ADVISORY - Your file contains fewer than 3 years.")
+    message("Where it exists, you should include at least 3 years of data in your file.")
+    message("This is to meet the upcoming change in accessibility legislation.")
     assign("three_years_result", "Advisory", envir = .GlobalEnv)
   } else {
     message("PASS - Your file contains at 3 years or more years of data.")
