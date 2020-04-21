2b06bb386db865d4cba412cc584a2826b7c3eda0
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-04-21 12:30:34 +0100

    package list

--- run.R
+++ run.R
@@ -1,4 +1,21 @@
 ## Running the Screener
+# using ugbly force to make renv read dependencies
+
+
+library(gitignore)
+library(rmarkdown)
+library(installr)
+library(readr)
+library(govdown)
+library(knitr)
+library(tidyr)
+library(stringr)
+library(dplyr)
+library(svDialogs)
+library(janitor)
+library(renv)
+
+
 
 # Setup your environment by running the following line
 source("setup_functions/setup_environment.R", encoding = "UTF-8")
