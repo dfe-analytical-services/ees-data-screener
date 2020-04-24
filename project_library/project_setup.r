a37afcdb7093f6e758397ef8b479287ed6e8753e
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-04-24 18:34:01 +0100

    adding the project library)

---
+++ project_library/project_setup.r
@@ -0,0 +1,13 @@
+# Project setup
+
+if (!require(here)) {
+  install.packages("here")
+  library(here)
+} 
+
+project_library <- paste0(here(),"/project_library/library")
+
+.libPaths(project_library)
+
+# Setup your environment by running the following line
+source("setup_functions/setup_environment.R", encoding = "UTF-8")
