2b2481309882e7b7074eadc5fa5c2fa5aac72d85
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-04-29 20:34:09 +0100

    switching away from here to avoid errors

--- project_library/project_setup.r
+++ project_library/project_setup.r
@@ -1,13 +1,6 @@
 # Project setup
 
-if (!require(here)) {
-  install.packages("here")
-  library(here)
-}
-
-project_library <- paste0(here(), "/project_library/library")
-
-.libPaths(project_library)
+project_library <- paste0(getwd(), "/project_library/library")
 
 # Setup your environment by running the following line
 source("setup_functions/setup_environment.R", encoding = "UTF-8")
