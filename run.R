121a6f9f32e96b449b8c521f1907fb3af9420d7a
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-05-05 14:46:28 +0100

    making the packages load quietly

--- run.R
+++ run.R
@@ -1,7 +1,7 @@
 ## Running the Screener
 
-# The following line will load the packages you need into your environment to run the screener
-suppressPackageStartupMessages(environment_setup())
+# The following line will quietly load the packages you need into your environment to run the screener
+suppressWarnings(suppressPackageStartupMessages(environment_setup()))
 
 # Screen your files by running the following line and responding to the prompts.
 screening_results()
