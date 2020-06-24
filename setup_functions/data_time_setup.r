3000294a1b5eacdb5a3bbed694dc3326d9ac99e2
RACE <Cameron.RACE@EDUCATION.GOV.UK>
2020-06-24 12:21:11 +0100

    adding Week 1 through 52 as time identifiers

--- setup_functions/data_time_setup.r
+++ setup_functions/data_time_setup.r
@@ -87,6 +87,21 @@ time_period <- function(data) {
           time_period_preresult <- TRUE
         }
       }
+      
+      if ((base_identifier %in% c("Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10",
+                                  "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16", "Week 17", "Week 18", "Week 19", "Week 20", 
+                                  "Week 21", "Week 22", "Week 23", "Week 24", "Week 25", "Week 26", "Week 27", "Week 28", "Week 29", "Week 30", 
+                                  "Week 31", "Week 32", "Week 33", "Week 34", "Week 35", "Week 36", "Week 37", "Week 38", "Week 39", "Week 40", 
+                                  "Week 41", "Week 42", "Week 43", "Week 44", "Week 45", "Week 46", "Week 47", "Week 48", "Week 49", "Week 50", 
+                                  "Week 51", "Week 52")) == TRUE) {
+        if ((nrow(filter(time_length, digits == 4)) == nrow(time_length)) == FALSE) {
+          message("FAIL - the time period for weeks must be a four digit number.")
+          time_period_preresult <- FALSE
+        } else {
+          message("PASS - time period is the correct number of digits for the time_identifer.")
+          time_period_preresult <- TRUE
+        }
+      }
 
       if ((base_identifier %in% c("Calendar year")) == TRUE) {
         if ((nrow(filter(time_length, digits == 4)) == nrow(time_length)) == FALSE) {
@@ -230,6 +245,20 @@ time_identifier_mix <- function(data) {
   }
   if ((base_identifier %in% c("Tax year")) == TRUE) {
     lev <- c("Tax year")
+  }
+  
+  if ((base_identifier %in% c("Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10",
+                              "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16", "Week 17", "Week 18", "Week 19", "Week 20", 
+                              "Week 21", "Week 22", "Week 23", "Week 24", "Week 25", "Week 26", "Week 27", "Week 28", "Week 29", "Week 30", 
+                              "Week 31", "Week 32", "Week 33", "Week 34", "Week 35", "Week 36", "Week 37", "Week 38", "Week 39", "Week 40", 
+                              "Week 41", "Week 42", "Week 43", "Week 44", "Week 45", "Week 46", "Week 47", "Week 48", "Week 49", "Week 50", 
+                              "Week 51", "Week 52")) == TRUE) {
+    lev <- c("Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10",
+             "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16", "Week 17", "Week 18", "Week 19", "Week 20", 
+             "Week 21", "Week 22", "Week 23", "Week 24", "Week 25", "Week 26", "Week 27", "Week 28", "Week 29", "Week 30", 
+             "Week 31", "Week 32", "Week 33", "Week 34", "Week 35", "Week 36", "Week 37", "Week 38", "Week 39", "Week 40", 
+             "Week 41", "Week 42", "Week 43", "Week 44", "Week 45", "Week 46", "Week 47", "Week 48", "Week 49", "Week 50", 
+             "Week 51", "Week 52")
   }
 
   if (time_identifier_result == FALSE) {
--- setup_functions/pre-defined_variables.r
+++ setup_functions/pre-defined_variables.r
@@ -5,7 +5,13 @@
 acceptable_time_identifiers <- c(
   "Spring term", "Autumn term", "Autumn and spring term", "Summer term",
   "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
-  "Calendar year", "Financial year", "Academic year", "Tax year", "Reporting year"
+  "Calendar year", "Financial year", "Academic year", "Tax year", "Reporting year",
+  "Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10",
+  "Week 11", "Week 12", "Week 13", "Week 14", "Week 15", "Week 16", "Week 17", "Week 18", "Week 19", "Week 20", 
+  "Week 21", "Week 22", "Week 23", "Week 24", "Week 25", "Week 26", "Week 27", "Week 28", "Week 29", "Week 30", 
+  "Week 31", "Week 32", "Week 33", "Week 34", "Week 35", "Week 36", "Week 37", "Week 38", "Week 39", "Week 40", 
+  "Week 41", "Week 42", "Week 43", "Week 44", "Week 45", "Week 46", "Week 47", "Week 48", "Week 49", "Week 50", 
+  "Week 51", "Week 52"
 )
 
 acceptable_observational_units <- c(
